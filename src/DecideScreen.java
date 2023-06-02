1a56c31692a7ecfcbb0819b233ffcd0f7eadba63
piotr <piotr.koneczny.02@wp.pl>
2023-06-02 13:30:27 +0200

    Nowa klasa, w której zrobię okno decyzji i jego funkcjonalności - DecideScreen

---
+++ src/DecideScreen.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+public class DecideScreen {
+    public DecideScreen() {
+        JFrame mainGUIFrame = new JFrame("Wordle");
+    }
+
+    public static void main(String[] args){
+        new MainScreen();
+    }
+}
